c5e93a3ad4fbe4961a0f8ed1529c718c2e524a72
ysakava <yuliya.sakava@idt.net>
2020-03-11 12:27:24 +0300

    hometask

---
+++ src/main/java/lesson5/MyHomeTaskLesson5.java
@@ -0,0 +1,34 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class MyHomeTaskLesson5 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter a:");
+        String ReadFromConsoleA = scanner.nextLine();
+        System.out.print("Please, enter b:");
+        String ReadFromConsoleB = scanner.nextLine();
+        Integer ConverterStringA = Integer.parseInt(ReadFromConsoleA);
+        Integer ConverterStringB = Integer.parseInt(ReadFromConsoleB);
+
+        Integer result1 = plus(ConverterStringA, ConverterStringB);
+        Integer result2 = minus(ConverterStringA, ConverterStringB);
+        print("Вы сложили числа " + ConverterStringA + " и "+ ConverterStringB + " и сумма равнa " + result1);
+        print("Вы вычли из числа " + ConverterStringA + " число "+ ConverterStringB + " и разница равнa " + result2);
+    }
+
+    public static Integer plus (Integer firstParameter1, Integer secondParameter1) {
+       Integer sum = firstParameter1+secondParameter1;
+        return sum;
+    }
+    public static Integer minus (Integer firstParameter2, Integer secondParameter2) {
+        Integer sub = firstParameter2-secondParameter2;
+        return sub;
+    }
+
+    public static void print (String result) {
+        System.out.println(result);
+    }
+
+}
---
+++ src/main/java/lesson5/MyMainClass.java
@@ -0,0 +1,31 @@
+package lesson5;
+
+
+import java.util.Scanner;
+
+public class MyMainClass {
+
+   /* public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter a:");
+        String ReadFromConsoleA = scanner.nextLine();
+        System.out.print("Please, enter b:");
+        String ReadFromConsoleB = scanner.nextLine();
+        result = union(animal, name, symbol);
+        print(result);
+    }
+
+    public static Integer plus (String firstParameter, String secondParameter, char symbol) {
+        String result = firstParameter+symbol+secondParameter;
+        return result;
+    }
+    public static Integer minus (String firstParameter, String secondParameter, char symbol) {
+        String result = firstParameter+symbol+secondParameter;
+        return result;
+    }
+
+    public static void print (String printNew) {
+        System.out.print(printNew);
+    }*/
+
+}
